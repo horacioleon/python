f47a3bb2cae5d550eb2404cd7e5c8a82483038ed
Horacio Leon <horacio.leon@gmail.com>
2018-06-04 15:45:35 -0300

    adding logging
    add logging to /var/log/zabbix/zabbix_deleted_hosts.log

--- delete_hosts_disabled.py
+++ delete_hosts_disabled.py
@@ -4,6 +4,7 @@ import json
 import requests
 import time
 import datetime
+import logging
 
 def zabbix_api(url,payload):
 	req = urllib2.Request(url, payload.encode(), {"Content-type":"application/json"})
@@ -84,6 +85,8 @@ pass_api = "$API_PASSWORD"
 
 token = get_token()
 group_id="$GROUP_ID DISABLED HOST"
+hosts_deleted = 0
+logging.basicConfig(format='%(asctime)s - %(message)s', datefmt='%m-%d-%Y %H:%M:%S', filename='/var/log/zabbix/zabbix_deleted_hosts.log',level=logging.DEBUG)
 retention = (datetime.date.today() - datetime.timedelta(days=7)).strftime('%s')
 saida = get_host_from_group(token,group_id)
 
@@ -92,3 +95,7 @@ for i  in saida:
 	date_hw_decomm =  float(get_decommissioned(token,host_id)[0]['inventory']['date_hw_decomm'])
 	if date_hw_decomm > float(retention):
 		delete_host(token,host_id)
+		hosts_deleted = hosts_deleted + 1
+
+if hosts_deleted == 0:
+	logging.info("No hosts were deleted")
